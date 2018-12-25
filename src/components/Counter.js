ffd8ff76d7c5f63c26a61f61b69e98ad630ca8a8
best <1404076435@qq.com>
2018-12-25 11:08:14 +0800

    feat：实现左侧生命周期钩子

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
 
 export default class Counter extends Component{
+  static defaultProps = { // 默认属性对象
+    count: 0
+  }
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {count: this.props.count};
+  }
+  componentWillMount () {
+    console.log('1、组件将要挂载：componentWillMount')
+  }
+  handleClick = () => {
+    this.setState(oldState => ({count: oldState.count + 1}))
   }
   render() {
+    console.log('2、开始渲染组件：render')
     return (
       <div>
-        root
+        {this.state.count}
+        <button onClick={this.handleClick}>+</button>
       </div>
     )
+  }
+  componentDidMount () {
+    console.log('3、组件挂载完成：componentDidMount')
   }
 }
